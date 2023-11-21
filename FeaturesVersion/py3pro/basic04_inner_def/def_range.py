b93f9df865626fc4a8a4b595824604f51b9cf5f4
SteveRocket <rocket_2014@126.com>
2023-11-22 00:57:10 +0800

    调整目录结构

--- FeaturesVersion/py3pro/basic04_inner_def/def_range.py
+++ FeaturesVersion/py3pro/basic04_inner_def/def_range.py
@@ -1,8 +1,27 @@
 # range()函数的使用示例
+print(list(range(-9)))  # []
+print(list(range(0, -9)))  # []
+print(list(range(0)))  # []
+print(list(range(0, -9, -1)))  # [0, -1, -2, -3, -4, -5, -6, -7, -8]
+# 输出0到10之间的偶数
+print(list(range(0, 10, 2)))  # [0, 2, 4, 6, 8]
+
+
+elements = ["I", "am", "steverocket"]
+for index, element in enumerate(elements):
+    print(index, element)
+
+for i in range(len(elements)):
+    print(i, elements[i])
+
+
 # 生成从0开始，到5（不包含5）的整数序列
 for i in range(5):
     print(i, end=' ')  # 输出：0 1 2 3 4
 print()
+
+
+
 
 # 生成从2开始，到5（不包含5）的整数序列
 for i in range(2, 5):
